df962fc192afb0f8085c2e3eff3a42ca5281956a
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-02-12 15:19:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -259,7 +259,7 @@ async def remove_user(client, message: Message):
     except ValueError:
         await message.reply_text("Invalid command format. Use: /removeuser <user_id>")
 
-YOUR_ADMIN_ID = 1147534909
+YOUR_ADMIN_ID = 6077444526
 
 # Helper function to check admin privilege
 def is_admin(user_id):
